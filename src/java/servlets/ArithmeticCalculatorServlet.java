cc79d4a8ea4adb9baa330ff1a28110220317e9ac
amida <amida@10.0.0.83>
2022-09-25 11:09:12 -0600

    Finished Arithmetic Calculator

--- src/java/servlets/ArithmeticCalculatorServlet.java
+++ src/java/servlets/ArithmeticCalculatorServlet.java
@@ -1,5 +1,6 @@
+package servlets;
+
 import java.io.IOException;
-import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -10,10 +11,53 @@ public class ArithmeticCalculatorServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        getServletContext().getRequestDispatcher("/WEB-INF/arithmeticcalculator.jsp")
+                .forward(request, response);
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        String numFirst = request.getParameter("first");
+        String numSecond = request.getParameter("second");
+        String operation = request.getParameter("operation");
+        String result = "---";
+        
+        int firstNum = Integer.parseInt(numFirst);
+        int secNum = Integer.parseInt(numSecond);
+        
+        if(numFirst == null || numFirst.equals("") || numSecond == null || numSecond.equals("")){
+            request.setAttribute("resultMessage", "Result: INVALID");
+            getServletContext().getRequestDispatcher("/WEB-INF/arithmeticcalculator.jsp")
+                    .forward(request, response);
+            return;
+        }
+        
+        if(numFirst.equals("") && numSecond.equals("")){
+            request.setAttribute("resultMessage", "Result: ---");
+            getServletContext().getRequestDispatcher("/WEB-INF/arithmeticcalculator.jsp")
+                    .forward(request, response);
+            return;
+        }
+        
+        switch (operation) {
+            case "+":
+                result = Integer.toString(firstNum + secNum);
+                break;
+            case "-":
+                result = Integer.toString(firstNum - secNum);
+                break;
+            case "*":
+                result = Integer.toString(firstNum * secNum);
+                break;
+            case "%":
+                result = Integer.toString(firstNum % secNum);
+                break;
+            default:
+                break;
+        }
+        
+        request.setAttribute("result", result);
+        getServletContext().getRequestDispatcher("/WEB-INF/arithmeticcalculator.jsp").forward(request, response);
     }    
 }
--- web/WEB-INF/agecalculator.jsp
+++ web/WEB-INF/agecalculator.jsp
@@ -18,6 +18,6 @@
             <input type="submit" value="Your Age Next Year">
         </form>
             <p>${nextAge}</p>
-            <p> <a href=""> Arithmetic Calculator</a></p>
+            <p> <a href="arithmetic"> Arithmetic Calculator</a></p>
     </body>
 </html>
